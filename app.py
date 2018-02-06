daa1e5f703a32b3d481e9a1018bcae32125a8b15
Richie <pmc.computer@yahoo.com>
2018-02-06 20:13:53 +0700

    new

--- app.py
+++ app.py
@@ -1,42 +1,6 @@
 #!/usr/bin/python
 
-#import timer
+import timer
 
 
-#gui = timer.FullScreenApp()
-import csv
-class Csv():
-    def __init__(self,parent):
-        self.parent = parent
-        self.a=None
-        self.k = self.bacacsv()
-        pass
-
-    def kondisi(self):
-        if self.parent == self.k:
-            self.masukancsv()
-        else :
-            pass
-
-    def bacacsv(self):
-        with open('pass.csv', 'r') as csvfile:
-            spamreader = csv.reader(csvfile)#, delimiter=' ', quotechar='|')
-            for row in spamreader:
-                self.a = ', '.join(row)
-        return self.a
-    
-    def masukancsv(self):
-        if self.parent == self.bacacsv():
-            data =['ok']
-            with open("pass.csv", "w") as csv_file:
-                writer = csv.writer(csv_file)#, delimiter=',')
-                self.hasil = writer.writerow(data)
-        else :
-            pass
-        
-        return self.hasil
-        
-
-maink = Csv("admin")
-maink.kondisi()
-print (maink.bacacsv())
+gui = timer.FullScreenApp()
---
+++ csvfile.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+#import timer
+
+
+#gui = timer.FullScreenApp()
+import csv
+class Csv():
+    def __init__(self,parent):
+        self.parent = parent
+        self.a = None
+        pass
+
+    def kondisi(self):
+        if self.parent == self.bacacsv():
+            self.masukancsv()
+        else :
+            pass
+
+    def bacacsv(self):
+        with open('pass.csv', 'r') as csvfile:
+            spamreader = csv.reader(csvfile)#, delimiter=' ', quotechar='|')
+            for row in spamreader:
+                self.a = ', '.join(row)
+        return self.a
+    
+    def masukancsv(self):
+        if self.parent == self.bacacsv():
+            data =['ok']
+            with open("pass.csv", "w") as csv_file:
+                writer = csv.writer(csv_file)#, delimiter=',')
+                self.hasil = writer.writerow(data)
+        else :
+            pass
+        
+        return self.hasil
+        
+
+maink = Csv("admin")
+maink.kondisi()
